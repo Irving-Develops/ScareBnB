71870905f253df14e2be01bf087a923ef0dfde09
Irving Arreola Palacios <irving.a789@gmail.com>
2022-06-24 17:31:31 -0500

    created spot model

---
+++ backend/db/models/spot.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Spot = sequelize.define('Spot', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {model: 'Users'}
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {});
+  Spot.associate = function(models) {
+    // associations can be defined here
+    Spot.belongsTo(Model.User, {foreignKey: 'userId'})
+  };
+  return Spot;
+};
