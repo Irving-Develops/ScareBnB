50d3bdd3631230406fc34542ca41c712aa790b7f
Irving Arreola Palacios <irving.a789@gmail.com>
2022-06-26 16:47:30 -0500

    spot get route completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,10 +5,14 @@ const {Spot, Image} = require('../../db/models/');
 const router = express.Router();
 
 router.get('/', asyncHandler(async(req,res) => {
-    const spots = await Spot.findAll()
-    const images = await Image.findAll()
-    console.log(res.json(images))
-    console.log(res.json(spots))
+    const spots = await Spot.findAll({include: [Image]})
+    // const images = await Image.findAll()
+    // console.log(res.json(images))
+    // let data = res.json(spots);
+    //  data += await res.json(images);
+    console.log("WORKING =====================>", spots)
+    // data.map(spot=> console.log('=========>', spot[id]))
+    // console.log(res.json(spots).map(spot => {spot.id}))
     return res.json(spots)
 }))
 
