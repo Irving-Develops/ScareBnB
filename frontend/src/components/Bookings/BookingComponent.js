9157dd895baa24bdb201af05decbfa9d2ebaa6c0
Irving Arreola Palacios <irving.a789@gmail.com>
2022-07-01 15:11:16 -0500

    no bugs

--- frontend/src/components/Bookings/BookingComponent.js
+++ frontend/src/components/Bookings/BookingComponent.js
@@ -6,22 +6,73 @@ import {thunkGetBooking, thunkDeleteBooking, thunkCreateBooking} from '../../sto
 export default function BookingComponent({spotId}) {
     const selectorBookings = useSelector(state => state?.bookingReducer);
     const userId = useSelector(state => state.session.user.id);
-    const user = useSelector(state => state.session.user);
-
-    const bookingsArr = Object.values(selectorBookings)
     const history = useHistory();
     const dispatch = useDispatch();
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("")
     const [upToDate, setUpToDate] = useState(true)
+    const [errors, setErrors] = useState([])
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [day1, setDay1] = useState(new Date(endDate))
+    const [day2, setDay2] = useState(new Date(startDate))
+    const [bookingExist, setBookingExist] = useState(false)
+    
+    
+    const bookingsArr = Object.values(selectorBookings)
+    let bookingsForSpot = bookingsArr.filter(booking=> booking.spotId === spotId)
+
+    console.log('=========>' , bookingsForSpot.userId === userId)
+    
+    useEffect(() => {
+        setDay1(new Date(endDate));
+        setDay2(new Date(startDate))
+
+    }, [endDate, startDate])
 
     
     
-    let bookingsForSpot = bookingsArr.filter(booking=> booking.spotId === spotId)
+    useEffect(() => {
+        const err = [];
+        
+        //checking the number of days between two dates
+        const days = (day1, day2) => {
+            let difference = day1.getTime() - day2.getTime();
+            let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+            return TotalDays;
+        }
+        
+        let amountOfDays = days(day1, day2);
+        
+        if(amountOfDays > 28) err.push("Your booking cannot be longer thank 28 days")
+        
+        
+        const startDateArr = startDate.split('-')
+
+        let today = new Date()
+        let year = today.getFullYear();
+        let month = today.getMonth()
+        let day = today.getDate();
+
+
+        if(startDateArr[0] < year) err.push("Please enter a valid year")
+        if(startDateArr[0] <= year && startDateArr[1] < month + 1 ) err.push("Please enter a valid month")
+        if(startDateArr[0] <= year && startDateArr[1] <= month + 1 && startDateArr[2] < day) err.push("Please enter a valid day")
+        if(startDateArr[0] <= year && startDateArr[1] <= month + 1 && startDateArr[2] <= day) err.push("Cannot make a booking on the same day")
+        
+        if(startDate > endDate) err.push("Please enter a valid end date");
+        if(startDate < today) err.push("Please enter a valid start date");
+        if(startDate === endDate) err.push("Start date and end date cannot match");
+
+
+        return setErrors(err)
+    }, [startDate, endDate, day1, day2])
     
     const  onSubmit = async(e) =>{
         e.preventDefault();
 
+        setHasSubmitted(true);
+        if (errors.length) return alert('cannot submit')
+        
         const payload = {
             spotId,
             userId,
@@ -32,46 +83,78 @@ export default function BookingComponent({spotId}) {
         await dispatch(thunkCreateBooking(payload))
         await dispatch(thunkGetBooking(spotId))
         await setUpToDate(true)
+        await setHasSubmitted(false)
+        await setBookingExist(true)
     }
 
-    console.log("test", bookingsForSpot)
+    useEffect(() => {
+        if(!upToDate){
+            dispatch(thunkGetBooking(spotId))
+            setUpToDate(true)
+        }
+
+    }, [dispatch, upToDate, spotId])
+
 
     useEffect(() => {
         dispatch(thunkGetBooking(spotId))
+        setUpToDate(true)
     }, [dispatch, spotId])
 
 
     async function onDelete(booking) {
         await dispatch(thunkDeleteBooking(booking, history))
+        // await dispatch(thunkGetBooking(spotId))
         await setUpToDate(false)
+        await setHasSubmitted(false)
+        await setBookingExist(false)
     }
 
-    // if(upToDate === false) return null;
-    return (
-        <>
+    if(upToDate === false) return null;
+    // if(upToDate === true){
+        return (
+            <>
+    
+                    {bookingsForSpot && upToDate && bookingsForSpot.map(booking => (
+                        <div key={booking.id}>
+                            <p >Booking for user {booking?.userId} at spot {booking?.spotId} from {booking?.startDate} to {booking?.endDate}</p>
+                            {booking.userId === userId && (
+                                <button type="button" onClick={() => onDelete(booking)}> Delete Booking</button>
+                            )}
+                        </div>
+    
+                    ))}
+    
+                <div>
+                    <h2>create booking</h2>
+                    <form onSubmit={onSubmit}>
+                    {hasSubmitted && errors.length > 0 && (
+                                            <div>
+                        The following errors were found:
+                        <ul>
+                            {errors.map(error => (
+                            <li key={error}>{error}</li>
+                            ))}
+                        </ul>
+                        </div>
+                    )}
+                    {!bookingExist && (
+                        <>
+                            <label>Start</label>
+                            <input type="date" value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            />
+                            <input type="date"  value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)} 
+                            />
+                             <button>Submit</button>
+                        </>
+                        
+                        )}
+                    </form>
+                </div>
+            </>
+        )
 
-                {bookingsForSpot && upToDate && bookingsForSpot.map(booking => (
-                    <div key={booking.id}>
-                        <p >Booking for user {booking?.userId} at spot {booking?.spotId} from {booking?.startDate} to {booking?.endDate}</p>
-                        <button type="button" onClick={() => onDelete(booking)}> Delete Booking</button>
-                    </div>
-
-                ))}
-
-            <div>
-                <h2>create booking</h2>
-                <form onSubmit={onSubmit}>
-                    <label>Start</label>
-                    <input type="date" value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                    />
-                    <input type="date"  value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                    
-                    />
-                <button>Submit</button>
-                </form>
-            </div>
-        </>
-    )
+    // }
 }
