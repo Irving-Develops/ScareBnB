1f821e28d23b126cb29c9220629113da901836cf
Irving Arreola Palacios <irving.a789@gmail.com>
2023-05-02 18:32:30 -0500

    adding new seed data

--- backend/db/migrations/20220626005524-create-spot.js
+++ backend/db/migrations/20220626005524-create-spot.js
@@ -1,57 +1,54 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Vans', {
+    return queryInterface.createTable("Vans", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: {model: 'Users'}
+        references: { model: "Users" },
       },
-      address: {
+      make: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      city: {
+      model: { allowNull: false, type: Sequelize.STRING },
+      location: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
-      state: {
+      year: {
         allowNull: false,
-        type: Sequelize.STRING
-      },
-      country: {
-        allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER,
       },
       name: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       price: {
         allowNull: false,
-        type: Sequelize.DECIMAL(10,2)
+        type: Sequelize.DECIMAL(10, 2),
       },
-      history: {
+      description: {
         allowNull: false,
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Vans');
-  }
+    return queryInterface.dropTable("Vans");
+  },
 };
--- backend/db/seeders/20220626010513-vanSeeds.js
+++ backend/db/seeders/20220626010513-vanSeeds.js
@@ -1,112 +1,133 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-   return queryInterface.bulkInsert('Vans', [
-     {
-       userId: 1,
-       address: "1120 Westchester Pl, Country Club Park, Los Angeles, California ",
-       city: "Los Angeles",
-       state: "California",
-       country: "United States",
-       name: "The Murder House",
-       price: 499.99,
-       history: 'German-American architect Alfred Rosenheim built the house in 1902, and after a five year construction, used it as his own residence. The house is sited on a sloping tree-studded 3/4 acre lot at 1120 Westchester Place in Country Club Park. The surrounding neighborhood was known as "Billionaire Row" in 1908, with some of the wealthiest families in California residing there. Over 30 people have reportedly died in the house, their angry souls trapped forever within its walls. It is believed to have been haunted since the 1920s.',
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 2,
-       address: "1126 Queens Hwy, Long Beach, CA 90802",
-       city: "Long Beach",
-       state: "California",
-       country: "United States",
-       name: "The Queen Mary",
-       price: 649.99,
-       history: 'Queen Mary has been dubbed “One of the Top Ten Most Haunted Places on Earth” by Time Magazine.  As soon as you set foot aboard this once-glorious ship, you can feel that something’s just not quite right. Home to The White Lady, Little Jackie, John Henry, Grumpy, Captain Treasure Jones, John Peddar and Dana.',
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 3,
-       address: "8 Potters Pond, Wotton-under-Edge GL12 7HF, United Kingdom",
-       city: "Wotton-under-Edge",
-       state: "Gloucestershire",
-       country: "United Kingdom",
-       name: "Ancient Ram Inn",
-       price: 100.00,
-       history: 'This 12th-century inn is one of the oldest in the Western world. It is believed to have once been a pagan burial ground, and boasts that it is haunted by more than 20 spirits, including ghost children and a pagan high priestess. Fearless travelers can book a stay at the inn in hopes of experiencing the spirits themselves.',
-       createdAt: new Date(),
-       updatedAt: new Date(),
-     }, {
-       userId: 4,
-       address: "Burggasse 7, 93195 Wolfsegg, Germany",
-       city: "Wolfsegg",
-       state: "Bavaria",
-       country: "Germany",
-       name: "Burg Wolfsegg",
-       price: 1200,
-       history: 'This 800-year-old castle in the municipality of Wolfsegg, Germany, is apparently haunted by a woman who scares off any visitors who pass. She is rumored to be the ghost of Klara von Helfenstein, who was reportedly murdered by her jealous husband. The castle is now open for visitors!',
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 5,
-       address: "230 2nd St, Fall River, MA 02721",
-       city: "Fall River",
-       state: "Massachusetts",
-       country: "United States",
-       name: "Lizzie Borden House",
-       price: 279.00,
-       history: 'The lizzie Borden House is the sight of a gruesome and highly publicized murder that occurred in 1892. Although she was acquitted, Lizzie Borden was suspected of killing her father and stepmother with a hatchet in the unassuming home. Since then, guests have reported all manner of strange sightings in the house. Rooms are available for intrepid travelers who want to experience the notorious home for themselves.',
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 6,
-       address: "1138 Royal St, New Orleans, LA 70116",
-       city: "New Orleans",
-       state: "Louisiana",
-       country: "United States",
-       name: "LaLaurie's Mansion",
-       price: 400.00,
-       history: "For almost 200 years, there have been reports of paranormal activity coming from this house. It shouldn't surprise many that many hauntings are attributed to the slaves that Madame LaLaurie kept on the property. There is a room in the LaLaurie Mansion where slaves were often kept - and reports of moaning coming from that room are common. Phantom footsteps echo through the house with regularity. Many people who have stood near the house have reported feeling as if they were taken over by negative energy.",
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 7,
-       address: "1 RUE JEANNE SAY, 49320 Brissac-Loire-Aubance, France",
-       city: "Brissac-Loire-Aubance",
-       state: "Pays de la Loire",
-       country: "France",
-       name: "Château de Brissac",
-       price: 999.99,
-       history: "Dubbed as the 'Giant of the Loire Valley' this is the highest castle in France boasting seven floors, 204 rooms, numerous portrait galleries, and a private opera house which seats 200 people. It was a fortress built by the Counts of Anjou in the 11th Century. Visitors have reported seeing La Dame Verte, (Green Lady), who was apparently the illegitimate child of King Charles VII and was later murdered by her husband.",
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 8,
-       address: "1 Homestead Ln, Junee NSW 2663, Australia",
-       city: "Junee",
-       state: "New South Wales",
-       country: "Australia",
-       name: "Monte Cristo Homestead",
-       price: 300.00,
-       history: "Monte Cristo Homestead is one of Australia's most haunted homes. Perched on a hill overlooking Junee in the Riverina region of NSW, it certainly looks the part.vBuilt by the Crawley family in the 1880 s, it 's an imposing two - story Victorian mansion with high ceilings and ornate cast - iron balconies.",
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }, {
-       userId: 9,
-       address: "7747 US-61, St Francisville, LA 70775",
-       city: "St. Francisville",
-       state: "Louisiana",
-       country: "United States",
-       name: "The Myrtles Plantation",
-       price: 500.00,
-       history: "One of America's most haunted sites, this plantation is reportedly home to at least 12 different ghosts, including that of William Winter, who was murdered in the house. Come book a stay at this beautiful but unimaginable spooky plantation!",
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }
-   ], {});
+    return queryInterface.bulkInsert(
+      "Vans",
+      [
+        {
+          userId: 1,
+          name: "Adventurer's Dream",
+          make: "Ford",
+          model: "Transit",
+          year: 2019,
+          location: "1234 W Randolph St, Chicago, IL 60606",
+          description:
+            "This van is perfect for adventurers looking for a comfortable and convenient way to travel. It has a queen-sized bed, a kitchenette, and plenty of storage space.",
+          price: 150,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2,
+          name: "Luxury Cruiser",
+          make: "Mercedes-Benz",
+          model: "Sprinter",
+          year: 2020,
+          location: "1603 Orrington Ave, Evanston, IL 60201",
+          description:
+            "This luxurious van is perfect for those who want to travel in style. It has a full kitchen, bathroom, and a comfortable bed.",
+          price: 200,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3,
+          name: "Rugged Explorer",
+          make: "Ram",
+          model: "Promaster",
+          year: 2017,
+          location: "416 Hamilton Blvd, Peoria, IL 61602",
+          description:
+            "This rugged van is perfect for those who love the great outdoors. It has a comfortable bed, a kitchenette, and plenty of storage space for your gear.",
+          price: 120,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 4,
+          name: "Cozy Camper",
+          make: "Chevrolet",
+          model: "Express",
+          year: 2021,
+          location: "101 S Madison St, Bloomington, IL 61701",
+          description:
+            "This cozy van is perfect for a romantic getaway. It has a comfortable bed, a kitchenette, and a romantic ambience.",
+          price: 130,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 5,
+          name: "Surfer's Paradise",
+          make: "Ford",
+          model: "E-Series",
+          year: 2017,
+          location: "1260 Meadow Rd, Northbrook, IL 60062",
+          description:
+            "This van is perfect for surfers looking for a convenient way to hit the waves. It has a comfortable bed, a kitchenette, and plenty of storage space for your boards.",
+          price: 140,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 6,
+          name: "Family Cruiser",
+          make: "Dodge",
+          model: "Grand Caravan",
+          year: 2022,
+          location: "188 W Grand Ave, Elmhurst, IL 60126",
+          description:
+            "This van is perfect for families looking for a comfortable and convenient way to travel. It has a queen-sized bed, a kitchenette, and a child-friendly interior.",
+          price: 160,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 7,
+          name: "Vintage Explorer",
+          make: "Volkswagen",
+          model: "Bus",
+          year: 1972,
+          location: "123 Main St, Urbana, IL 61801",
+          description:
+            "This vintage van is perfect for those who want to travel back in time. It has a comfortable bed, a retro interior, and plenty of charm.",
+          price: 180,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 8,
+          name: "The Prairie Rover",
+          make: "Ford",
+          model: "Econoline",
+          year: 2016,
+          location: "456 Lake Shore Dr, Chicago, IL 60611",
+          description:
+            "This beautifully converted van is perfect for those who want to explore the Midwest. With a comfortable queen bed, a kitchenette, and plenty of storage, it has everything you need for an unforgettable adventure.",
+          price: 125,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 9,
+          name: "The Windy City Cruiser",
+          make: "Mercedes-Benz",
+          model: "Sprinter",
+          year: 2018,
+          location: "789 Elm St, Evanston, IL 60201",
+          description:
+            "Experience the ultimate luxury with this sleek and modern Sprinter van. Equipped with a full kitchen, a luxurious queen bed, and a bathroom, it's the perfect home on wheels for your next road trip.",
+          price: 275,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-   return queryInterface.bulkDelete('Vans', null, {});
-  }
+    return queryInterface.bulkDelete("Vans", null, {});
+  },
 };
