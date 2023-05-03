da7a9c352e0b2d6f23d8a77def5de3e44e77328e
Irving Arreola Palacios <irving.a789@gmail.com>
2023-05-03 16:52:16 -0500

    added login

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import * as sessionActions from "../store/session";
+import { useDispatch } from "react-redux";
+// import "./LoginFormModal.css";
+// import "../../index.css";
+
+function LoginForm() {
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isActive, setIsActive] = useState(false);
+
+  const demo = () => {
+    setCredential("user1");
+    setPassword("password");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div id="form-wrapper">
+      <div id="form-header">
+        <p>Log In</p>
+      </div>
+
+      <form onSubmit={handleSubmit} id="form-content">
+        <h3 id="form-title">Welcome to ScareBnB</h3>
+        <div id="errors">
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+        </div>
+        <div id="input-container">
+          <input
+            style={{ backgroundColor: " white" }}
+            className={"form-input"}
+            type="text"
+            value={credential}
+            placeholder="Username or Email"
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+          <hr />
+          <input
+            style={{ backgroundColor: " white" }}
+            className={"form-input"}
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {/* </label> */}
+        </div>
+
+        <button type="submit">Log In</button>
+        <button type="submit" onClick={demo}>
+          Demo
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { thunkGetAllSpots } from '../store/spots'
+import LoginForm from '../components/LoginForm'
 
 const Home = () => {
     const dispatch = useDispatch()
     const spots = useSelector((state) => state.spots)
-
+    const user = useSelector(state => state)
     useEffect(() => {
         dispatch(thunkGetAllSpots())
     },[dispatch])
-    console.log(spots, "---------")
+    console.log(user, "---------")
   return (
     <section>
       <h2>HomePage</h2>
+      <LoginForm />
     </section>
     )
 }
